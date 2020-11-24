77a8743b85c8ec7a81fcdd0383092b499ed1172f
Ajara75 <ajararyspekova22@gmail.com>
2020-11-24 12:51:44 +0600

    react

--- ajara/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import './app.css'
-
-const App = () => {
-  const [name, setName] = useState('Ajara');
-  return <div className="app">[name]</div>
-}
-
-export default App;
